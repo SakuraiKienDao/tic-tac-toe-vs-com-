a483efc4983e875283c88335de71fb89c80e27be
SakuraiKienDao <sakuraikiendao@gmail.com>
2022-01-11 06:51:17 +0900

    first commit

---
+++ js/tic_tac_toe.js
@@ -0,0 +1,261 @@
+"use strict";
+
+let flag =true;
+
+let counter=9;
+
+const squares =document.getElementsByClassName("square");
+const squaresArray = Array.from(squares);
+
+const a_1= document.getElementById("a_1");
+const a_2= document.getElementById("a_2");
+const a_3= document.getElementById("a_3");
+const b_1= document.getElementById("b_1");
+const b_2= document.getElementById("b_2");
+const b_3= document.getElementById("b_3");
+const c_1= document.getElementById("c_1");
+const c_2= document.getElementById("c_2");
+const c_3= document.getElementById("c_3");
+
+const mgstxt1 = '<p class="image"><img src="img/penguins.jpg" width = 61px height = 61px></p><p class="text">Penguins Attack!(your turn)</p>'
+const mgstxt2 = '<p class="image"><img src="img/whitebear.jpg" width = 61px height = 61px></p><p class="text">Bear Attack!(com turn)</p>'
+const mgstxt3 = '<p class="image"><img src="img/penguins.jpg" width = 61px height = 61px></p><p class="text animate_animated animate_lightSpeedInRight">Penguins Win</p>'
+const mgstxt4 = '<p class="image"><img src="img/whitebear.jpg" width = 61px height = 61px></p><p class="text animate_animated animate_lightSpeedInRight">Bear Win</p>'
+const mgstxt5 = '<p class="image"><img src="img/penguins.jpg" width = 61px height = 61px><img src="img/whitebear.jpg" width = 61px height = 61px></p><p class="text animate_bounceIn">Draw!!!</p>'
+
+let gameSound = ["sound/click_sound1.mp3", "sound/click_sound2.mp3", "sound/penwin_sound.mp3", "sound/bearwin_sound.mp3", "sound/draw_sound.mp3"];
+
+window.addEventListener("DOMContentLoaded",
+    function(){
+        setMessage("pen-turn");
+
+        squaresArray.forEach(square=>{
+            square.classList.add("js-clickable")
+        })
+    },false
+);
+
+//newgame
+const newgamebtn_display= document.getElementById("newgame-btn");
+const newgamebtn= document.getElementById("btn90");
+
+//Win or Loose Jud
+const line1 = JudgLine(squaresArray, ["a_1","a_2","a_3"]);
+const line2 = JudgLine(squaresArray, ["b_1","b_2","b_3"]);
+const line3 = JudgLine(squaresArray, ["c_1","c_2","c_3"]);
+const line4 = JudgLine(squaresArray, ["a_1","b_1","c_1"]);
+const line5 = JudgLine(squaresArray, ["a_2","b_2","c_2"]);
+const line6 = JudgLine(squaresArray, ["a_3","b_3","c_3"]);
+const line7 = JudgLine(squaresArray, ["a_1","b_2","c_3"]);
+const line8 = JudgLine(squaresArray, ["a_3","b_2","c_1"]);
+
+const lineArray = [line1, line2, line3, line4, line5, line6, line7, line8];
+
+let winningLine = null;
+
+function JudgLine(targetArray, idArray){
+    return targetArray.filter(function(e){
+        return (e.id === idArray[0] || e.id === idArray[1] || e.id === idArray[2] );
+    });
+}
+//win judment
+
+squaresArray.forEach(square=>{
+    square.addEventListener("click", ()=>{
+        let gammeOverFlg = isSelect(square);
+
+        if(gammeOverFlg===0){
+            const squaresBox = document.getElementById("squaresBox");
+            squaresBox.classList.add("js-unclickable");
+            setTimeout(() => {
+                bearTurn();
+            }, 2000);
+        }
+    })
+})
+
+
+function isSelect(selectSquare){
+    let gameOverFlg= "0";
+    if(flag === true){
+        let music =new Audio(gameSound[0]);
+        music.currentTime= 0;
+        music.play();
+
+        selectSquare.classList.add("js-pen-checked");
+        selectSquare.classList.add("js-unclickable");
+        selectSquare.classList.add("js-clickable");
+        //penWin
+        if(isWinner("penguins")){
+            setMessage("pen-win");
+            gameOver("penguins");
+            return gameOverFlg ="1";
+        }
+
+        setMessage("bear-turn");
+        flag= false;
+    }else{
+        let music =new Audio(gameSound[1]);
+        music.currentTime= 0;
+        music.play();
+
+        selectSquare.classList.add("js-bear-checked");
+        selectSquare.classList.add("js-unclickable");
+        selectSquare.classList.add("js-clickable");
+
+        //bearWin
+        if(isWinner("bear")){
+            setMessage("bear-win");
+            gameOver("bear")
+            return gameOverFlg ="1";
+        }
+
+        setMessage("pen-turn");
+        flag=true;
+    }
+
+    counter--;
+
+    if(counter===0){
+        setMessage("draw");
+        gameOver("draw");
+        return gameOverFlg="1";
+    }
+    return gameOverFlg= "0"
+}
+
+function isWinner(symbol){
+    const result= lineArray.some(function(line){
+        const subResult= line.every(function(square){
+            if(symbol==="penguins"){
+                return square.classList.contains("js-pen-checked");
+            }
+            if(symbol==="bear"){
+                return square.classList.contains("js-bear-checked");
+            }
+        });
+        if(subResult){winningLine = line}
+        return subResult;
+    })
+    return result;
+}
+
+function setMessage(id){
+    const msg = document.getElementById("msgtext");
+
+    switch (id) {
+        case "pen-turn": {
+            msg.innerHTML = mgstxt1;
+            break;
+        }
+        case "bear-turn": {
+            msg.innerHTML = mgstxt2;
+            break;
+        }
+        case "pen-win": {
+            msg.innerHTML = mgstxt3;
+            break;
+        }
+        case "bear-win": {
+            msg.innerHTML = mgstxt4;
+            break;
+        }
+        case "draw":{
+            msg.innerHTML= mgstxt5;
+            break;
+        }
+        default:
+            document.getElementById("msgtext").innerHTML=mgstxt1;
+    }
+}
+
+
+function gameOver( status ){
+
+    let w_sound
+    switch (status) {
+        case "penguins":
+            w_sound = gameSound[2];
+            break;
+        case "bear" :
+            w_sound = gameSound[3];
+            break;
+    
+        case "draw":
+            w_sound= gameSound[4];
+            break;
+    }
+    let music =new Audio(w_sound);
+        music.currentTime= 0;
+        music.play();
+    
+    // squaresArray.forEach(function(square){
+    //     return square.classList.add("js-unclickable");
+    // });
+
+    squaresBox.classList.add("js-unclickable");
+
+
+    //show go btn
+    newgamebtn_display.classList.remove("js-hidden");
+
+    if (status=== "penguins"){
+        if(winningLine){
+            winningLine.forEach(square=>{
+            return square.classList.add("js-pen_highLight");
+        }
+            )};
+        
+        $(document).snowfall({
+            flakeColor : "rgb(255,240,245)",
+            maxSpeed : 3,
+            minSpeed : 1,
+            maxSize  : 20,
+            minSize  : 10,
+            round : true
+        });
+
+    } else if (status=== "bear"){
+        if(winningLine){
+            winningLine.forEach(square=>{
+            return square.classList.add("js-bear_highLight");
+        })};
+        
+        $(document).snowfall({
+            flakeColor : "rgb(255,240,245)",
+            maxSpeed : 3,
+            minSpeed : 1,
+            maxSize  : 20,
+            minSize  : 10,
+            round : true
+        });
+    }
+    }
+
+
+    newgamebtn.addEventListener("click", ()=>{
+        flag= true;
+        counter =9;
+        winningLine = null;
+        squaresArray.forEach(square=>{
+            if(square.classList.contains("js-pen-checked")) square.classList.remove("js-pen-checked");
+            if(square.classList.contains("js-bear-checked")) square.classList.remove("js-bear-checked");
+            if(square.classList.contains("js-unclickable")) square.classList.remove("js-unclickable");
+            if(square.classList.contains("js-pen_highLight")) square.classList.remove("js-pen_highLight");
+            if(square.classList.contains("js-bear_highLight")) square.classList.remove("js-bear_highLight");
+            square.classList.add("js-clickable")
+        });
+        squaresBox.classList.remove("js-unclickable")
+        setMessage("pen-turn");
+        newgamebtn_display.classList.add("js-hidden")
+        $(document).snowfall("clear");
+    })
+
+//com play\
+
+function bearTurn(){
+    gameOverFlg= "0";
+    const bearSquare =squaresArray.filter(square=>{
+        if(square.classList.contains("js-clickable")) return bearSquare;
+    })
+}
